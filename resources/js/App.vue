70af416525e49b35cd8462adea0f5a109bf1b4e8
Thunderbird93 <antircristian@gmail.com>
2023-01-31 16:17:25 +0100

    api (prova)

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,12 +1,42 @@
+<script>
+
+import axios from 'axios';
+
+export default {
+    name:'App',
+    data(){
+        return{
+            baseUrl: 'http://127.0.0.1:8000/api/',
+            posts: [],
+            contentMaxLenght: 150,
+            pagination:{
+                current: 1,
+                last: null
+            }
+        }
+    },
+    methods:{
+        getApi(){
+            axios.get(this.baseUrl + 'posts', {
+                params:{
+                    page: this.pagination.current
+                }
+            })
+                .then(result =>{
+                    this.posts = result.data.posts.data;
+                    this.pagination.last = result.data.posts.last_page
+                })
+        }
+    }
+    components:{
+        Header
+    }
+}
+</script>
+
 <template>
   
 </template>
-
-<script>
-export default {
-
-}
-</script>
 
 <style>
 
